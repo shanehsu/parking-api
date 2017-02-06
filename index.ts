5280d9004afdc091fb8b0f4439067801d1103e75
Hsu Peng Jun <hsu.pengjun@icloud.com>
2017-02-06 14:04:00 +0800

    測試 mLab 連線

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express = require('express')
 import body_parser = require('body-parser')
+import mongodb = require('mongodb')
 
 let app = express()
 app.use(body_parser.json())
@@ -8,8 +9,20 @@ app.set('port', (process.env.PORT || 5000))
 app.use('/app', express.static(__dirname + '/app'))
 app.use('/admin', express.static(__dirname + '/admin'))
 
-app.get('/', (req, res) => {
-  res.send("Hello World!")
+app.get('/', async (req, res) => {
+  // 試圖連線至 MongoDB
+  let uri = process.env.MONGODB_URI
+  let client = new mongodb.MongoClient()
+  try {
+    let connection = await client.connect(uri)
+    res.send("Hello World!")
+    connection.close()
+  } catch (err) {
+    res.json({
+      "message": "資料庫連線失敗",
+      "raw": err
+    })
+  }
 })
 
 app.listen(app.get('port'), () => {
