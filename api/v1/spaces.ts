0c625bf08a5afd852fc236b77fda1d00244c47fc
Hsu Peng Jun <hsu.pengjun@icloud.com>
2017-02-28 23:56:07 +0800

    Added query middleware. Implement spaces in query middleware.

--- api/v1/spaces.ts
+++ api/v1/spaces.ts
@@ -1,65 +1,56 @@
 import { Router } from 'express'
 import { ApplicationError } from './../../util/error'
 import { Grids, ConsecutiveGrids, Grid } from './util/grid'
+import { query } from './util/query'
 import { createRandom } from './../../util/random'
 
 export let spacesRouter = Router()
 
-spacesRouter.get('/', (req, res, next) => {
-  try {
-    // 路徑需要 `grids` 查詢選項
-    if (!req.query.grids) {
-      throw new ApplicationError(`缺少必要的查詢選項 "grids"`, 400)
-    }
+spacesRouter.get('/',
+  query('available', false, 'boolean'),
+  query('serial', false, 'boolean'),
+  query('grids', true, 'custom', (value: string) => Grids.parse(value)),
+  async (req, res, next) => {
+    try {
+      let grids: Grids = req.query.grids
 
-    // 處理 blocks
-    let grids = Grids.parse(req.query.grids)
-    if (grids.grids.length > 100) {
-      throw new ApplicationError(`要求超過 100 個方格。`, 400)
-    }
-    let query = {
-      "$or": grids.consecutiveGrids.map(consecutiveGrid => {
-        return {
-          "longitude": {
-            "$and": [{ "$gte": consecutiveGrid.longitude }, { "$lt": consecutiveGrid.longitude + consecutiveGrid.longitudeGridSpan * 0.1 }]
-          },
-          "latitude": {
-            "$and": [{ "$gte": consecutiveGrid.latitude }, { "$lt": consecutiveGrid.latitude + consecutiveGrid.latitudeGridSpan * 0.1 }]
+      if (grids.grids.length > 100) {
+        throw new ApplicationError(`要求超過 100 個方格。`, 400)
+      }
+      let gridsQuery = {
+        "$or": grids.consecutiveGrids.map(consecutiveGrid => {
+          return {
+            "longitude": {
+              "$and": [{ "$gte": consecutiveGrid.longitude }, { "$lt": consecutiveGrid.longitude + consecutiveGrid.longitudeGridSpan * 0.1 }]
+            },
+            "latitude": {
+              "$and": [{ "$gte": consecutiveGrid.latitude }, { "$lt": consecutiveGrid.latitude + consecutiveGrid.latitudeGridSpan * 0.1 }]
+            }
           }
-        }
-      })
+        })
+      }
+
+      // TODO: Move this to query(), use the `default` setting
+      let available: boolean = req.query.available === undefined ? false : req.query.available
+      let availableQuery = {
+        "available": available
+      }
+
+      // TODO: Move this to query(), use the `default` setting
+      let serial: boolean = req.query.serial === undefined ? false : req.query.serial
+
+      let result: any = null
+
+      try {
+        result = await req.db.collection('spaces').find({
+          "$and": [gridsQuery, availableQuery]
+        }, !serial ? { serial: 0 } : undefined).toArray()
+      } catch (err) {
+        throw new ApplicationError('資料庫錯誤', 500, err)
+      }
+
+      res.json(result)
+    } catch (err) {
+      next(err)
     }
-
-
-    //Peter
-    if (!req.query.available && !req.query.serial) {
-      let result = req.db.collection('spaces').find(query, {}).toArray().then(
-        result => res.json(result)
-      )
-      res.json(result)
-    } else if (req.query.available && !req.query.serial) {
-      let result = req.db.collection('spaces').find(query, [{ available: { "$eq": true } }, { serial: { "$eq": false } }]).toArray().then(
-        result => res.json(result)
-      )
-
-    } else if (!req.query.avaible && req.query.serial) {
-      let result = req.db.collection('spaces').find(query, [{ available: { "$eq": false } }, { serial: { "$eq": true } }]).toArray().then(
-        result => res.json(result)
-      )
-      res.json(result)
-    } else if (req.query.avaible && req.query.serial) {
-      let result = req.db.collection('spaces').find(query, [{ available: { "$eq": true } }, { serial: { "$eq": true } }]).toArray().then(
-        result => res.json(result)
-      )
-      res.json(result)
-    } else {
-      let result = "無符合資料"
-      throw new ApplicationError(`無符合資料`, 400)
-    }
-    //PeterEnd
-
-    //let result = req.db.collection('spaces').find(query, {}).toArray()
-  } catch (err) {
-    next(err)
-  }
-})
+  })
