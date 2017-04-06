7a33e3100295460f00742ab32baa6667c5b5fcea
sugarPeter886 <bulss8516@gmail.com>
2017-04-07 00:14:01 +0800

    and test again..

--- api/v1/v1Router.ts
+++ api/v1/v1Router.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import { spacesRouter } from './spaces'
 import { loginRouter } from './login/loginRouter'
 import { sensorRouter } from './sensor/sensorRouter'
+
 export let v1Router = Router()
 
 v1Router.use('/spaces', spacesRouter)
